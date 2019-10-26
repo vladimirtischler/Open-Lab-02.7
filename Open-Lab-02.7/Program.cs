d30f25c14c2a1e99df1a06ed889f29b6c986ca5d
vladimirtischler <tischler.vladimir@gmail.com>
2019-10-26 19:00:08 +0200

    open lab

---
+++ Open-Lab-02.7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Open_Lab_02._7
+{
+    class Program
+    {
+        public static bool IsTimeForMilkAndCookies(int year, int month, int day)
+        {
+            return (month == 12 && day == 24);
+           
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter year!");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter month!");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter day!");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(IsTimeForMilkAndCookies(a, b, c));
+        }
+    }
+}
